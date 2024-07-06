dfd70e472e00a00d3b780a8b1666b8b84c976ac9
Rastislav <vojtusrasto1@gmail.com>
2024-07-06 14:41:36 +0200

    XSLT.java rework

--- InformalMDD/src/main/java/cz/cuni/mff/vojtusr/xslt/XSLT.java
+++ InformalMDD/src/main/java/cz/cuni/mff/vojtusr/xslt/XSLT.java
@@ -3,23 +3,23 @@ package cz.cuni.mff.vojtusr.xslt;
 import javax.xml.transform.*;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 
 public class XSLT {
-    private final File xsltFile;
-    public XSLT(File xsltFile) {
+    private final String xsltFile;
+    public XSLT(String xsltFile) {
         this.xsltFile = xsltFile;
     }
-    public void transform(File input, File output) throws TransformerException, FileNotFoundException {
+    public void transform(String input, String output) throws TransformerException, FileNotFoundException {
+        Source xsltSource = new StreamSource(xsltFile);
+
         TransformerFactory factory = TransformerFactory.newInstance();
 
-        Transformer transformer = factory.newTransformer(new StreamSource(xsltFile));
+        Transformer transformer = factory.newTransformer(xsltSource);
 
         StreamSource source = new StreamSource(input);
 
-        StreamResult result = new StreamResult(new FileOutputStream(output));
+        StreamResult result = new StreamResult(output);
 
         transformer.transform(source, result);
     }
