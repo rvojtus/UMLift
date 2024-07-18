35c1c116a705c4dc766290ccc22469b57db14756
Rastislav <vojtusrasto1@gmail.com>
2024-07-18 12:04:37 +0200

    add basic GenModel generation from Ecore

---
+++ InformalMDD/src/main/java/cz/cuni/mff/vojtusr/emf/GenModelGenerate.java
@@ -0,0 +1,47 @@
+package cz.cuni.mff.vojtusr.emf;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+import java.io.IOException;
+import java.util.Collections;
+
+
+public class GenModelGenerate {
+    public void generate() throws IOException {
+        // Initialize resource set
+        ResourceSet resourceSet = new ResourceSetImpl();
+
+        // Register resource set
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+
+        // Load the Ecore model
+        URI ecoreURI = URI.createFileURI("InformalMDD/src/main/resources/test.ecore");
+        Resource ecoreResource = resourceSet.getResource(ecoreURI, true);
+        EPackage ecorePackage = (EPackage) ecoreResource.getContents().getFirst();
+
+        // Create a GenModel
+        GenModel genModel = GenModelFactory.eINSTANCE.createGenModel();
+        genModel.setModelName("ModelName");
+        genModel.setModelDirectory("src/main/java");
+        genModel.setModelPluginID("plugin.id");
+        genModel.initialize(Collections.singleton(ecorePackage));
+
+        // Save the GenModel
+        URI genmodelURI = URI.createFileURI("InformalMDD/src/main/resources/test.genmodel");
+        final XMIResourceImpl genModelResource = new XMIResourceImpl(genmodelURI);
+        genModelResource.getDefaultSaveOptions().put(XMLResource.OPTION_ENCODING, "UTF-8");
+        genModelResource.getContents().add(genModel);
+        genModelResource.save(Collections.EMPTY_MAP);
+
+        System.out.println("Genmodel created successfully.");
+    }
+}
