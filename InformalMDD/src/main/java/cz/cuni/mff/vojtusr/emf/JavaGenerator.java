3ec4a00ff6363e45a1c926b6092856b2aff5e0a4
Rastislav <vojtusrasto1@gmail.com>
2024-07-31 09:35:30 +0200

    fixed Code Generation - added mapping

---
+++ InformalMDD/src/main/java/cz/cuni/mff/vojtusr/emf/JavaGenerator.java
@@ -0,0 +1,94 @@
+package cz.cuni.mff.vojtusr.emf;
+
+import org.eclipse.emf.codegen.ecore.generator.Generator;
+import org.eclipse.emf.codegen.ecore.generator.GeneratorAdapterFactory;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
+import org.eclipse.emf.codegen.ecore.genmodel.generator.GenBaseGeneratorAdapter;
+import org.eclipse.emf.common.util.BasicMonitor;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.ecore.*;
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.emf.codegen.ecore.genmodel.generator.GenModelGeneratorAdapterFactory;
+
+/**
+ * Generates Java code from provided Ecore and GenModel files, using EMF libraries
+ */
+public class JavaGenerator {
+
+    /**
+     * Generates Java code from provided GenModel
+     * @param genModel
+     * @throws java.io.IOException
+     */
+    public void generate(GenModel genModel) throws java.io.IOException {
+        Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("genmodel", new XMIResourceFactoryImpl());
+        EPackage.Registry.INSTANCE.put(GenModelPackage.eNS_URI, GenModelPackage.eINSTANCE);
+
+        GeneratorAdapterFactory.Descriptor.Registry.INSTANCE.addDescriptor(
+                GenModelPackage.eNS_URI, GenModelGeneratorAdapterFactory.DESCRIPTOR
+        );
+
+        genModel.reconcile();
+        genModel.setCanGenerate(true);
+        genModel.setValidateModel(true);
+        genModel.setForceOverwrite(true);
+        final String rootContainer = "testProject";// todo
+        final String rootPath = "/Users/rastislav.vojtus/Documents/Java_PRG/2324/2024-vojtus/InformalMDD/src/main/resources";// todo
+        EcorePlugin.getPlatformResourceMap().put(rootContainer, URI.createFileURI(rootPath));
+
+
+        // Create the generator
+        Generator generator = new Generator();
+        generator.setInput(genModel);
+
+        // Generate the model code
+        Diagnostic diagnostic = generator.generate
+                (genModel, GenBaseGeneratorAdapter.MODEL_PROJECT_TYPE, "model project",
+                        new BasicMonitor.Printing(System.out));
+
+        if (diagnostic.getSeverity() == Diagnostic.ERROR) {
+            System.err.println(diagnostic);
+        }
+        else {
+            System.out.println("Code generation complete.");
+        }
+    }
+
+    /**
+     * Prints content of an Ecore Package - For debugging purposes
+     * @param ePackage
+     */
+    private static void printEPackageContents(EPackage ePackage) {
+        System.out.println("EPackage Name: " + ePackage.getName());
+        System.out.println("EPackage Namespace URI: " + ePackage.getNsURI());
+        System.out.println("EPackage Namespace Prefix: " + ePackage.getNsPrefix());
+        System.out.println();
+
+        for (EClassifier eClassifier : ePackage.getEClassifiers()) {
+            if (eClassifier instanceof EClass) {
+                EClass eClass = (EClass) eClassifier;
+                System.out.println("EClass Name: " + eClass.getName());
+
+                // Print super types
+                for (EClass superType : eClass.getESuperTypes()) {
+                    System.out.println("  Super Type: " + superType.getName());
+                }
+
+                // Print structural features
+                for (EStructuralFeature feature : eClass.getEStructuralFeatures()) {
+                    System.out.println("  Feature: " + feature.getName() + " (Type: " + feature.getEType().getName() + ")");
+                    if (feature instanceof EReference) {
+                        EReference reference = (EReference) feature;
+                        System.out.println("    Reference: " + reference.getName() + " -> " + reference.getEReferenceType().getName());
+                    }
+                }
+
+                System.out.println();
+            }
+        }
+    }
+}
