62afb1a478ab82c6e534c89bdcaed52d6633c956
Rastislav <vojtusrasto1@gmail.com>
2024-09-22 20:02:41 +0200

    add a simple Form to a ViewPart for Eclipse MDD plugin

--- Eclipse-plugin/InformalMDD-plugin/src/main/java/cz/cuni/mff/vojtusr/SwingViewPart.java
+++ Eclipse-plugin/InformalMDD-plugin/src/main/java/cz/cuni/mff/vojtusr/SwingViewPart.java
@@ -1,15 +1,28 @@
 package cz.cuni.mff.vojtusr;
 
+import cz.cuni.mff.vojtusr.emf.JavaGenerator;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
+
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 
 public class SwingViewPart extends ViewPart {
     public static final String ID = "cz.cuni.mff.vojtusr.swingview";
+
+    private static String projectTitle = "Project title";
+    private static String projectDir = "";
 
     public SwingViewPart() {
 
@@ -17,15 +30,87 @@ public class SwingViewPart extends ViewPart {
 
     @Override
     public void createPartControl(org.eclipse.swt.widgets.Composite parent) {
-        Button button = new Button(parent, SWT.PUSH);
-        button.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
-        button.setText("Hello World");
-        button.addSelectionListener(new SelectionAdapter() {
+
+        GridLayout layout = new GridLayout(2, false);
+        parent.setLayout(layout);
+
+        Label projectTitleLabel = new Label(parent, SWT.NONE);
+        projectTitleLabel.setText("Project title: ");
+
+        Text projectTitleText = new Text(parent, SWT.BORDER);
+        projectTitleText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+
+        Label xsltFileLabel = new Label(parent, SWT.NONE);
+        xsltFileLabel.setText("XSLT file: ");
+
+        Text xsltFilePathText = getXsltFilePathText(parent);
+
+
+        Button generateButton = new Button(parent, SWT.PUSH);
+        generateButton.setText("Generate Code");
+        GridData buttonGridData = new GridData(SWT.CENTER, SWT.CENTER, false, false);
+        buttonGridData.horizontalSpan = 2;
+        generateButton.setLayoutData(buttonGridData);
+
+        generateButton.addSelectionListener(new SelectionAdapter() {
+            @Override
             public void widgetSelected(SelectionEvent e) {
-                MessageDialog.openInformation(parent.getShell(), "Title", "Hello World");
+                projectTitle = projectTitleText.getText();
+
+                String xsltFilePath = xsltFilePathText.getText();
+
+                //startCodeGeneration(xsltFilePath);
+            }
+
+        });
+    }
+
+    private static Text getXsltFilePathText(Composite parent) {
+        Text xsltFilePathText = new Text(parent, SWT.BORDER);
+        xsltFilePathText.setEditable(false);
+        xsltFilePathText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+
+        Button selectXSLTFileButton = new Button(parent, SWT.PUSH);
+        selectXSLTFileButton.setText("Select File...");
+        selectXSLTFileButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                FileDialog fileDialog = new FileDialog(parent.getShell(), SWT.OPEN);
+                fileDialog.setText("Select a file");
+                fileDialog.setFilterExtensions(new String[]{"*.xsl", "*.xml"});
+                fileDialog.setFilterNames(new String[]{"XSL File", "XML File"});
+                String filePath = fileDialog.open();
+                if (filePath != null) {
+                    xsltFilePathText.setText(filePath);
+                }
             }
         });
+        return xsltFilePathText;
+    }
 
+    private static String getCurrentDirectory() {
+        IWorkbenchPage iWorkbenchPage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        ISelection selection = iWorkbenchPage.getSelection();
+        if (selection instanceof IStructuredSelection) {
+            Object element = ((IStructuredSelection) selection).getFirstElement();
+            if (element instanceof IResource) {
+                IResource resource = (IResource) element;
+                return resource.getLocation().toOSString();
+            }
+        }
+        return null;
+    }
+
+    private static void startCodeGeneration(String xsltFile, String inputUMLFile) {
+        JavaGenerator javaGenerator = new JavaGenerator(xsltFile);
+        try {
+            javaGenerator.generate(inputUMLFile, projectDir, projectTitle);
+        } catch (IOException e) { // todo better error display
+            System.err.println("IOException occurred: " + e.getMessage());
+        } catch (TransformerException e) {
+            System.err.println("TransformerException occurred: " + e.getMessage());
+        }
     }
 
     @Override
