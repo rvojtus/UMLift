9d276ae2f94d322853bee0d68b19c792a9888b62
Rastislav <vojtusrasto1@gmail.com>
2024-10-31 09:02:45 +0100

    add compliance level JDK130 for GenModel

--- InformalMDD-core/src/main/java/cz/cuni/mff/vojtusr/emf/GenModelGenerate.java
+++ InformalMDD-core/src/main/java/cz/cuni/mff/vojtusr/emf/GenModelGenerate.java
@@ -1,14 +1,18 @@
 package cz.cuni.mff.vojtusr.emf;
 
+import org.eclipse.emf.codegen.ecore.genmodel.GenJDKLevel;
+import org.eclipse.emf.codegen.ecore.genmodel.GenModelPackage;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModel;
 import org.eclipse.emf.codegen.ecore.genmodel.GenModelFactory;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
 import java.io.IOException;
@@ -33,12 +37,13 @@ public class GenModelGenerate {
         GenModel genModel = GenModelFactory.eINSTANCE.createGenModel();
         genModel.setModelName(projectName);
         genModel.setModelDirectory(projectName + "/java");
+        genModel.setComplianceLevel(GenJDKLevel.JDK130_LITERAL);
         genModel.initialize(Collections.singleton(ecorePackage));
 
         // Save the GenModel
         URI genmodelURI = URI.createFileURI(resourcesPath + "/genmodel.genmodel");// todo maybe change file name
         final XMIResourceImpl genModelResource = new XMIResourceImpl(genmodelURI);
-        genModelResource.getDefaultSaveOptions().put(XMLResource.OPTION_ENCODING, "UTF-8");
+        genModelResource.getDefaultSaveOptions().put(XMIResource.OPTION_ENCODING, "UTF-8");
         genModelResource.getContents().add(genModel);
         genModelResource.save(Collections.EMPTY_MAP);
 
