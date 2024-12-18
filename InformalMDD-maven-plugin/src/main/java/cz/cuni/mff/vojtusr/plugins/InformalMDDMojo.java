f5c3e7ea9a22dbb9b3618351dcfb0b178bd29cbc
Rastislav <vojtusrasto1@gmail.com>
2024-12-18 10:07:18 +0100

    add code generation support

--- InformalMDD-maven-plugin/src/main/java/cz/cuni/mff/vojtusr/plugins/InformalMDDMojo.java
+++ InformalMDD-maven-plugin/src/main/java/cz/cuni/mff/vojtusr/plugins/InformalMDDMojo.java
@@ -9,7 +9,9 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
-@Mojo(name = "InformalMDD", defaultPhase = LifecyclePhase.COMPILE)
+import java.io.IOException;
+
+@Mojo(name = "InformalMDD")
 public class InformalMDDMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
@@ -20,11 +22,45 @@ public class InformalMDDMojo extends AbstractMojo {
     @Parameter(property = "projectDir")
     private String projectDir;
 
-    @Parameter(property = "UMLetFile")
+    @Parameter(property = "umletFile")
     private String UMLetFile;
+
+    @Parameter(property = "projectName", defaultValue = "projectTest")
+    private String projectName;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Ecore: " + ecoreFile + " ProjectDir: " + projectDir + " UMLetFile: " + UMLetFile);
+        getLog().info("Ecore: " + ecoreFile + " ProjectDir: " + projectDir +
+                " ProjectName: " + projectName + " UMLetFile: " + UMLetFile);
+
+        if (ecoreFile == null && UMLetFile == null) {
+            throw new MojoExecutionException("Missing ecoreFile and UMLetFile parameters");
+        }
+        if (projectDir == null) {
+            projectDir = project.getBasedir().getAbsolutePath() + "/";
+        }
+        if (UMLetFile != null) {
+            try {
+                generateFromUMLet();
+            } catch (IOException e) {
+                throw new MojoExecutionException("Error while generating java code", e);
+            }
+            return;
+        }
+        try {
+            generateJava();
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error while generating java code", e);
+        }
+    }
+
+    private void generateJava() throws IOException {
+        JavaGenerator javaGenerator = new JavaGenerator();
+        javaGenerator.generateCodeFromFiles(ecoreFile, projectDir);
+    }
+
+    private void generateFromUMLet() throws IOException {
+        JavaGenerator javaGenerator = new JavaGenerator();
+        javaGenerator.generateCodeFromUMLetFile(UMLetFile, projectDir, projectName);
     }
 }
