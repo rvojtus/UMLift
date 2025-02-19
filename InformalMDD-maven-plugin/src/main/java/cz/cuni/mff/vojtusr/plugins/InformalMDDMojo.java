390c695869db612d54b862c7ffee67022e3982cb
Rastislav <vojtusrasto1@gmail.com>
2025-02-19 22:06:01 +0100

    updated to use new method signatures

--- InformalMDD-maven-plugin/src/main/java/cz/cuni/mff/vojtusr/plugins/InformalMDDMojo.java
+++ InformalMDD-maven-plugin/src/main/java/cz/cuni/mff/vojtusr/plugins/InformalMDDMojo.java
@@ -1,5 +1,6 @@
 package cz.cuni.mff.vojtusr.plugins;
 
+import cz.cuni.mff.vojtusr.emf.CodeGenerationConfig;
 import cz.cuni.mff.vojtusr.emf.JavaGenerator;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -7,22 +8,24 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.eclipse.emf.common.util.Diagnostic;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 @Mojo(name = "InformalMDD")
 public class InformalMDDMojo extends AbstractMojo {
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     private MavenProject project;
 
-    @Parameter(property = "ecoreFile")
-    private String ecoreFile;
-
     @Parameter(property = "projectDir")
     private String projectDir;
 
+    @Parameter(property = "ecoreFile")
+    private String ecoreFile;
+
     @Parameter(property = "umletFile")
-    private String UMLetFile;
+    private String inputUMLetFile;
 
     @Parameter(property = "projectName", defaultValue = "projectTest")
     private String projectName;
@@ -36,15 +39,15 @@ public class InformalMDDMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("Ecore: " + ecoreFile + " ProjectDir: " + projectDir +
-                " ProjectName: " + projectName + " UMLetFile: " + UMLetFile);
+                " ProjectName: " + projectName + " UMLetFile: " + inputUMLetFile);
 
-        if (ecoreFile == null && UMLetFile == null) {
-            throw new MojoExecutionException("Missing ecoreFile and UMLetFile parameters");
+        if (ecoreFile == null && inputUMLetFile == null) {
+            throw new MojoExecutionException("Need to specify either Ecore File or UMLet File!");
         }
         if (projectDir == null) {
             projectDir = project.getBasedir().getAbsolutePath() + "/";
         }
-        if (UMLetFile != null) {
+        if (inputUMLetFile != null) {
             try {
                 generateFromUMLet();
             } catch (IOException e) {
@@ -53,19 +56,28 @@ public class InformalMDDMojo extends AbstractMojo {
             return;
         }
         try {
-            generateJava();
+            generateFromEcore();
         } catch (IOException e) {
             throw new MojoExecutionException("Error while generating java code", e);
         }
     }
 
-    private void generateJava() throws IOException {
+    private void generateFromEcore() throws IOException {
         JavaGenerator javaGenerator = new JavaGenerator();
-        javaGenerator.generateCodeFromEcore(ecoreFile, projectDir);
+        Diagnostic diagnostic = javaGenerator.generateCodeFromEcore(Path.of(ecoreFile), Path.of(projectDir));
     }
 
     private void generateFromUMLet() throws IOException {
         JavaGenerator javaGenerator = new JavaGenerator();
-        javaGenerator.generateCodeFromUMLetFile(UMLetFile, projectDir, projectName, NsPrefix, NsURI);
+        CodeGenerationConfig config = CodeGenerationConfig.getInstance()
+                .setInputUMLetFile(Path.of(inputUMLetFile))
+                .setGeneratedFilesDir(Path.of(projectDir))
+                .setOutputEcoreFile(Path.of(projectDir + "/ecore.ecore"))
+                .setOutputGenModelFile(Path.of(projectDir + "/genmodel.genmodel"))
+                .setProjectName(projectName)
+                .setProjectNsPrefix(NsPrefix)
+                .setProjectNsURI(NsURI)
+                .build();
+        Diagnostic diagnostic = javaGenerator.generateCodeFromUMLetFile(config);
     }
 }
